7c3b4cad008205176950dea6c8081d3824a47ef4
정채은 <jce0497@naver.com>
2023-01-26 11:18:54 +0900

    feat: Add mypage 플레이리스트 다이얼로그

--- frontend/src/views/mypage/mypage.vue
+++ frontend/src/views/mypage/mypage.vue
@@ -1,8 +1,8 @@
 <template>
   <v-template>
     <v-row class="outbox">
-      <v-col col="6" class="mypage_box">
-        <h2 style="text-align:left;"><b>My Page</b></h2>
+      <v-col col="6" md="3" lg="6" class="mypage_box">
+        <h2 style="text-align: left"><b>My Page</b></h2>
         <v-row>
           <v-col col="3" class="user_image">
             <img class="user_ex" src="../../assets/images/user_ex.png" />
@@ -23,115 +23,42 @@
           </v-col>
         </v-row>
       </v-col>
-      <v-col col="6" class="myplaylist_box">
+      <v-col col="6" md="3" lg="6" class="myplaylist_box">
         <v-row>
-          <h2 style="text-align:left;"><b>My Playlist</b><v-icon>mdi-music</v-icon></h2>
-          <v-dialog
-            v-model="add_dialog"
-            persistent
-            max-width="600px"
-          >
-            <template v-slot:activator="{ on}">
+          <h2 style="text-align: left">
+            <b>My Playlist</b><v-icon>mdi-music</v-icon>
+          </h2>
+          <v-dialog v-model="add_dialog" persistent max-width="600px">
+            <template v-slot:activator="{ on }">
               <v-btn
                 color="aliceblue"
                 v-on="on"
                 class="plus"
-                @click="add_dialog=true"
+                @click="add_dialog = true"
               >
                 +
               </v-btn>
             </template>
             <v-card>
               <v-card-title>
-                <span class="text-h5">User Profile</span>
+                <span class="text-h5" style="margin:20px;">Search</span>
               </v-card-title>
               <v-card-text>
                 <v-container>
-                  <v-row>
-                    <v-col
-                      cols="12"
-                      sm="6"
-                      md="4"
-                    >
-                      <v-text-field
-                        label="Legal first name*"
-                        required
-                      ></v-text-field>
-                    </v-col>
-                    <v-col
-                      cols="12"
-                      sm="6"
-                      md="4"
-                    >
-                      <v-text-field
-                        label="Legal middle name"
-                        hint="example of helper text only on focus"
-                      ></v-text-field>
-                    </v-col>
-                    <v-col
-                      cols="12"
-                      sm="6"
-                      md="4"
-                    >
-                      <v-text-field
-                        label="Legal last name*"
-                        hint="example of persistent helper text"
-                        persistent-hint
-                        required
-                      ></v-text-field>
-                    </v-col>
-                    <v-col cols="12">
-                      <v-text-field
-                        label="Email*"
-                        required
-                      ></v-text-field>
-                    </v-col>
-                    <v-col cols="12">
-                      <v-text-field
-                        label="Password*"
-                        type="password"
-                        required
-                      ></v-text-field>
-                    </v-col>
-                    <v-col
-                      cols="12"
-                      sm="6"
-                    >
-                      <v-select
-                        :items="['0-17', '18-29', '30-54', '54+']"
-                        label="Age*"
-                        required
-                      ></v-select>
-                    </v-col>
-                    <v-col
-                      cols="12"
-                      sm="6"
-                    >
+                    <v-card-text>
                       <v-autocomplete
-                        :items="['Skiing', 'Ice hockey', 'Soccer', 'Basketball', 'Hockey', 'Reading', 'Writing', 'Coding', 'Basejump']"
-                        label="Interests"
-                        multiple
+                        placeholder="노래 혹은 가수를 검색해주세요"
+                        prepend-icon="mdi-magnify"
+                        return-object
                       ></v-autocomplete>
-                    </v-col>
-                  </v-row>
+                    </v-card-text>
+                    <!-- <v-divider></v-divider> -->
                 </v-container>
-                <small>*indicates required field</small>
               </v-card-text>
               <v-card-actions>
                 <v-spacer></v-spacer>
-                <v-btn
-                  color="blue darken-1"
-                  text
-                  @click="add_dialog = false"
-                >
+                <v-btn color="blue darken-1" text @click="add_dialog = false">
                   Close
-                </v-btn>
-                <v-btn
-                  color="blue darken-1"
-                  text
-                  @click="add_dialog = false"
-                >
-                  Save
                 </v-btn>
               </v-card-actions>
             </v-card>
@@ -139,10 +66,10 @@
         </v-row>
       </v-col>
     </v-row>
-    <br>
-    <hr>
-    <br>
-    <h2 style="text-align:left;"><b>My Highlight</b></h2>
+    <br />
+    <v-divider></v-divider>
+    <br />
+    <h2 style="text-align: left"><b>My Highlight</b></h2>
   </v-template>
 </template>
 
@@ -150,8 +77,8 @@
 export default {
   name: "MypageBox",
   data: () => ({
-      add_dialog: false,
-    }),
+    add_dialog: false,
+  }),
 };
 </script>
 
@@ -160,28 +87,28 @@ export default {
   margin-top: 20px;
 }
 
-.user_image{
+.user_image {
   margin-top: 10px;
   padding-right: 0px;
   padding-top: 15px;
 }
 
-.user_ex{
+.user_ex {
   height: 170px;
-  background-color:aliceblue
+  background-color: aliceblue;
 }
 
-.user_info{
+.user_info {
   margin-top: 20px;
   /* line-height: 50px; */
 }
 
-.item_name{
-  margin:20px;
+.item_name {
+  margin: 20px;
   /* font-size: 1rem; */
 }
 
-.item_value{
+.item_value {
   background-color: aliceblue;
   margin: 20px;
   width: 130px;
@@ -193,9 +120,9 @@ export default {
   text-align: left;
 } */
 
-.plus{
+.plus {
   background-color: aliceblue;
-  border-radius: 50px;;
+  border-radius: 50px;
   height: 30px;
   margin-top: 3px;
   margin-left: 15px;
