3a9f90471665464227732378789a362d3b0784c4
seonkyu kim <iwon4u@naver.com>
2023-01-30 21:23:46 +0900

    feat: s3 적용

--- singsingtime/src/main/java/com/ssafy/api/controller/OpenviduController.java
+++ singsingtime/src/main/java/com/ssafy/api/controller/OpenviduController.java
@@ -1,12 +1,19 @@
 package com.ssafy.api.controller;
 
+import com.ssafy.api.service.DiaryService;
+import com.ssafy.db.entity.Diary;
 import io.openvidu.java.client.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
 import java.util.Map;
 
 @CrossOrigin(origins = "*")
@@ -56,6 +63,30 @@ public class OpenviduController {
 		ConnectionProperties properties = ConnectionProperties.fromJson(params).build();
 		Connection connection = session.createConnection(properties);
 		return new ResponseEntity<>(connection.getToken(), HttpStatus.OK);
+	}
+
+	@Autowired
+	DiaryService diaryService;
+	@ResponseBody   // Long 타입을 리턴하고 싶은 경우 붙여야 함 (Long - 객체)
+	@PostMapping(value="/diary/image",consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+	public Long saveDiary(HttpServletRequest request, @RequestParam(value="image") MultipartFile image, Diary diary) throws IOException {
+		System.out.println("DiaryController.saveDiary");
+		System.out.println(image);
+		System.out.println(diary);
+		System.out.println("------------------------------------------------------");
+		Long diaryId = diaryService.keepDiary(image, diary);
+		return diaryId;
+	}
+
+	@ResponseBody   // Long 타입을 리턴하고 싶은 경우 붙여야 함 (Long - 객체)
+	@PostMapping(value="/diary/video",consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+	public Long saveVideo(HttpServletRequest request, @RequestParam(value="video") MultipartFile video, Diary diary) throws IOException {
+		System.out.println("DiaryController.saveDiary");
+//		System.out.println(video);
+		System.out.println(diary);
+		System.out.println("------------------------------------------------------");
+		Long diaryId = diaryService.keepVideo(video, diary);
+		return diaryId;
 	}
 
 }
