ea511d3921f41539070f8999aceabe20037fc232
정채은 <jce0497@naver.com>
2023-02-01 08:34:08 +0900

    feat: Update 커뮤니티 컨텐츠 화면

--- frontend/src/views/contents/content.vue
+++ frontend/src/views/contents/content.vue
@@ -1,13 +1,35 @@
 <template>
-  <h3 style="margin-left: 30px"><b>어제자 광주 김범수 대결 영상 ㄷㄷ</b></h3>
-  <div justify="center" align="center">
+  <div justify="center" align="center" style="margin-top: 40px">
     <v-row>
-      <v-col lg="8">
-        <div style="width: 70%">영상 들어갈 자리</div>
+      <v-col lg="7">
+        <h3 align="start" style="margin-left: 80px;"><b>어제자 광주 김범수 대결 영상 ㄷㄷ</b></h3>
+        <video
+          src="../../assets/video/test.mp4"
+          width="700"
+          height="500"
+          autoplay
+        ></video>
+        <v-row justify="space-between">
+          <p style="margin-left:80px;">3,201 View</p>
+          <p style="margin-right:80px;">219 <v-icon>mdi-heart</v-icon></p>
+        </v-row>
       </v-col>
-      <v-col lg="3" class="comment">
-        <textarea name="" id="" cols="30" rows="10"></textarea>
-        <input type="text" />
+      <v-col lg="4" class="comment_box">
+        <v-row>
+          <h4><b>Comment</b></h4>
+          <input
+            class="comment_inpupt"
+            type="text"
+            placeholder=" 댓글을 달아 주세요"
+            v-model="comment"
+          />
+          <v-btn class="comment_create">+</v-btn>
+        </v-row>
+        <v-row justify="space-between" style="margin-top:20px;">
+          <p style="margin-left:5px;">내용</p>
+          <p style="margin-right:10px;">작성자</p>
+        </v-row>
+        <div class="comments_list"></div>
       </v-col>
     </v-row>
   </div>
@@ -16,12 +38,34 @@
 <script>
 export default {
   name: "ContentsBox",
+  data() {
+    return {
+      comment: "",
+    };
+  },
 };
 </script>
 
 <style>
-.comment {
-  border: 1px solid black;
-  height: 100%;
+.comment_box{
+  margin-top: 80px;
+}
+
+.comment_inpupt {
+  border-bottom: 1px solid rgb(0, 0, 0);
+  margin-right: 10px;
+  margin-top: 10px;
+  height: 30px;
+  width: 85%;
+}
+
+.comment_create {
+  border-radius: 20px;
+}
+
+.comments_list{
+  margin-top: 10px;
+  width: 100%;
+  height: 70%;
 }
 </style>
