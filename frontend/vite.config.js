4217ea3ec02c6e991747ed0aeff10efd1013fa08
DESKTOP-F4F7D3R\Kyungsam Kim <rudtka12@gmail.com>
2023-02-05 20:32:09 +0900

    feat:배포 개발환경 분리 작업

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,17 +1,21 @@
 import { fileURLToPath, URL } from "node:url";
-
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
 import vuetify from "vite-plugin-vuetify";
-
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue(), vuetify({ autoImport: true })],
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+
+export default({mode}) => {
+  process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+  return defineConfig({
+    plugins: [
+      vue(),vuetify({ autoImport: true }),
+      ],
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+      },
     },
-  },
-});
+  });
+}
