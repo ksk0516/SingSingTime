50aea5c216311fe1ea06855a2a486f0cc4cb3826
김선규 <iwon4u@naver.com>
2023-02-08 10:13:53 +0900

    fix: 화상 기능 수정적용

--- frontend/src/views/conferences/conference.vue
+++ frontend/src/views/conferences/conference.vue
@@ -200,7 +200,7 @@ props:{
 
     async createSession(sessionId) {
       const response = await axios.post(
-        import.meta.env.VITE_APP_URLE + "/api/v1/openvidu/sessions",
+        import.meta.env.VITE_APP_URL + "/api/v1/openvidu/sessions",
         { customSessionId: sessionId },
         {
           headers: { "Content-Type": "application/json" },
@@ -211,7 +211,7 @@ props:{
 
     async createToken(sessionId) {
       const response = await axios.post(
-        import.meta.env.VITE_APP_URLE + "/api/v1/openvidu/sessions/" + sessionId + "/connections",
+        import.meta.env.VITE_APP_URL + "/api/v1/openvidu/sessions/" + sessionId + "/connections",
         {},
         {
           headers: { "Content-Type": "application/json" },
